6234c029c8e0371a5d2db2f99f41149083888c51
Lucas Neris <lucasnerism@gmail.com>
2023-04-27 13:20:39 -0300

    Find single product route implemented

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,8 +1,10 @@
 import { database } from '../database/connect.js';
-const collection = database.collection('products')
+const collection = database.collection('products');
 
-const findAllProducts = () => collection.find({}).toArray()
+const findAllProducts = () => collection.find({}).toArray();
+const findOneProduct = (query) => collection.findOne(query);
 
 export default {
-    findAllProducts
-}
+    findAllProducts,
+    findOneProduct
+};
