a95793041b4e4ab9c695d5aecdd9aef465c4e575
Lucas Neris <lucasnerism@gmail.com>
2023-04-27 17:58:07 -0300

    Get cart route implemented

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,21 +1,31 @@
 import cartService from "../services/cart.service.js";
 
 async function postCart(req, res) {
-  const userId = req.headers.id;
+  const userId = req.id;
   const product = req.body;
 
   try {
     const userCart = await cartService.findCart({ userId });
     if (!userCart) await cartService.createCart({ userId, products: [product] });
     if (userCart) await cartService.insertOnCart({ userId }, product);
-    res.sendStatus(201);
+    res.status(201).json({ message: "Produto adicionado com sucesso!" });
   } catch (err) {
-    res.sendStatus(500);
+    res.status(500).json({ error: err.message });
+  }
+}
+
+async function getCart(req, res) {
+  const userId = req.id;
+  try {
+    const userCart = await cartService.findCart({ userId });
+    res.status(200).json({ message: "Ok!", userCart });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 }
 
 
 export default {
   postCart,
-
+  getCart
 };
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -4,5 +4,6 @@ import cartController from "../controllers/cart.controller.js";
 const cartRouter = Router();
 
 cartRouter.post('/cart', cartController.postCart);
+cartRouter.get('/cart', cartController.getCart);
 
 export default cartRouter;
