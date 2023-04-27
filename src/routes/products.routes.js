8af73d7cf82371118f8acb154e3c9b4d1fb5d725
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2023-04-27 17:54:39 -0300

    remove auth validation from get products route

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products.controller.js';
-import authMiddleware from '../middlewares/auth.middleware.js';
 import { validProduct } from '../middlewares/products.middlewares.js';
 
 const ProductsRouter = Router();
 
-ProductsRouter.get('/products', authMiddleware, ProductsController.findAll);
+ProductsRouter.get('/products', ProductsController.findAll);
 ProductsRouter.post('/products', validProduct, ProductsController.insertOne);
 ProductsRouter.get('/products/:slug', ProductsController.findOne);
 
